89303833fd89b430c9b1b935dd019373c9efcbcf
tokada <ubiqnet@gmail.com>
2013-06-20 23:33:03 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ohashisan::Application.config.secret_key_base = '7e5d51f8677aaf6752337600cdaa679e1c396c06986402b687bbb718e9b8bd472ffc6184d4d6f69430a5a0ea2a5bc0a592752afdcecc7151c4cc3e1c3a4f278b'
